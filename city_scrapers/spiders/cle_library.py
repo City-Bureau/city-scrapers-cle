ab9aa22e1574678a39f319f9fa2bca997f2e2f2a
pjsier <pjsier@gmail.com>
2019-09-11 18:29:59 -0500

    Add cle_library

---
+++ city_scrapers/spiders/cle_library.py
@@ -0,0 +1,136 @@
+import re
+from collections import defaultdict
+from datetime import datetime
+
+import scrapy
+from city_scrapers_core.constants import BOARD, COMMISSION, COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class CleLibrarySpider(CityScrapersSpider):
+    name = "cle_library"
+    agency = "Cleveland Public Library"
+    timezone = "America/Detroit"
+    allowed_domains = ["cpl.org"]
+    start_urls = [
+        "https://cpl.org/aboutthelibrary/board-of-trustees/monthly-board-meeting-minutes/"
+    ]
+    location = {
+        "name": "Louis Stokes Wing",
+        "address": "525 Superior Ave Cleveland, OH 44114",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        self.minutes_map = self._parse_minutes(response)
+        yield scrapy.Request(
+            "https://cpl.org/category/meeting/",
+            callback=self._parse_meetings_page,
+            dont_filter=True
+        )
+
+    def _parse_meetings_page(self, response):
+        """Parse meetings from results pages"""
+        yield from self._parse_meetings(response)
+
+        page_num = response.url.split("/")[-2]
+        if page_num.isdigit():
+            page_num = int(page_num) + 1
+        else:
+            page_num = 2
+        if page_num <= 3:
+            yield scrapy.Request(
+                "https://cpl.org/category/meeting/page/{}/".format(page_num),
+                callback=self._parse_meetings_page,
+                dont_filter=True
+            )
+
+    def _parse_meetings(self, response):
+        for item in response.css("article"):
+            title = self._parse_title(item)
+            summary = item.css(".entry-summary p:first-child::text").extract_first()
+            detail_link = item.css("a")[0].attrib["href"]
+            times = self._parse_times(summary)
+            if len(times) == 0:
+                continue
+            end = None
+            start = times[0]
+            if len(times) > 1:
+                end = times[1]
+            meeting = Meeting(
+                title=title,
+                description="",
+                classification=self._parse_classification(title),
+                start=start,
+                end=end,
+                all_day=False,
+                time_notes="",
+                location=self._parse_location(summary),
+                links=[{
+                    "title": "Agenda",
+                    "href": detail_link
+                }] + self.minutes_map[start.date()],
+                source=detail_link,
+            )
+
+            meeting["status"] = self._get_status(meeting, text=summary)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_minutes(self, response):
+        minutes_map = defaultdict(list)
+        for link in response.css(".entry-content a"):
+            link_str = " ".join(link.css("*::text").extract())
+            link_date_match = re.search(r"[a-zA-Z]{3,10} \d{1,2}, \d{4}", link_str)
+            if not link_date_match:
+                continue
+            link_date = datetime.strptime(link_date_match.group(), "%B %d, %Y").date()
+            minutes_map[link_date].append({
+                "title": "Minutes",
+                "href": response.urljoin(link.attrib["href"]),
+            })
+        return minutes_map
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        title_str = item.css(".entry-title a::text").extract_first().title()
+        if "Special" not in title_str:
+            return title_str.replace(" Meeting", "")
+        return title_str
+
+    def _parse_classification(self, title):
+        """Parse or generate classification from allowed options."""
+        if "Committee" in title:
+            return COMMITTEE
+        if "Commission" in title:
+            return COMMISSION
+        return BOARD
+
+    def _parse_times(self, summary):
+        """Parse start datetime as a naive datetime object."""
+        date_match = re.search(r"[a-zA-Z]{3,10} \d{1,2},? \d{4}", summary)
+        if not date_match:
+            return []
+        date_str = date_match.group().replace(",", "")
+        time_strs = re.findall(r"(\d{1,2}:\d{2} ([apmAPM\.]{2,4}|Noon))", summary)
+        time_objs = []
+        for time_str, _ in time_strs:
+            time_str = time_str.replace(".", "").replace("Noon", "pm").lower()
+            time_objs.append(datetime.strptime(" ".join([date_str, time_str]), "%B %d %Y %I:%M %p"))
+        return time_objs
+
+    def _parse_location(self, summary):
+        """Parse or generate location."""
+        if "Lake Shore" in summary:
+            return {
+                "name": "",
+                "address": "17109 Lake Shore Blvd Cleveland, OH 44110",
+            }
+        return self.location
---
+++ tests/test_cle_library.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import BOARD, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.cle_library import CleLibrarySpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "cle_library_meetings.html"),
+    url="https://cpl.org/category/meeting/",
+)
+test_minutes_response = file_response(
+    join(dirname(__file__), "files", "cle_library.html"),
+    url="https://cpl.org/category/meeting/",
+)
+spider = CleLibrarySpider()
+
+freezer = freeze_time("2019-09-11")
+freezer.start()
+
+spider.minutes_map = spider._parse_minutes(test_minutes_response)
+parsed_items = [item for item in spider._parse_meetings(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 4
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Special Board Meeting"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 8, 29, 12, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "cle_library/201908291200/x/special_board_meeting"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://cpl.org/special-board-meeting-4/"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [{
+        "href": "https://cpl.org/special-board-meeting-4/",
+        "title": "Agenda"
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+
+
+def test_all_day():
+    assert parsed_items[0]["all_day"] is False
