0f53db9b51c62373d778d44a61c22dd97887ad5e
pjsier <pjsier@gmail.com>
2019-09-25 15:46:58 -0500

    Add cuya_emergency_services_advisory

---
+++ city_scrapers/spiders/cuya_emergency_services_advisory.py
@@ -0,0 +1,44 @@
+import re
+
+from city_scrapers_core.constants import ADVISORY_COMMITTEE, COMMITTEE
+from city_scrapers_core.spiders import CityScrapersSpider
+
+from city_scrapers.mixins import CuyaCountyMixin
+
+
+class CuyaEmergencyServicesAdvisorySpider(CuyaCountyMixin, CityScrapersSpider):
+    name = "cuya_emergency_services_advisory"
+    agency = "Cuyahoga County Emergency Services Advisory Board"
+    start_urls = ["http://bc.cuyahogacounty.us/en-US/CC-EmergencySrvcsAdvsryBrd.aspx"]
+
+    def _parse_title(self, response):
+        title_str = " ".join([
+            w.strip()
+            for w in response.css("#contentColumn h1::text").extract_first().strip().split(" - ")
+            if "cancel" not in w.lower()
+        ])
+        return title_str.replace(" Meeting", "").strip()
+
+    def _parse_classification(self, title):
+        if "Committee" in title:
+            return COMMITTEE
+        return ADVISORY_COMMITTEE
+
+    def _parse_location(self, response):
+        loc_str = super()._parse_location(response)
+        split_loc = re.split(r"[-, ]{1,2}(?=\d{2})", loc_str, 1)
+        loc_name = ""
+        if len(split_loc) > 1:
+            loc_name = split_loc[0]
+            loc_addr = " ".join(split_loc[1:]).replace("Ohio", "OH")
+        else:
+            loc_addr = loc_str.replace("Ohio", "OH")
+        if "OH" not in loc_addr:
+            if "Independence" not in loc_addr:
+                loc_addr += " Cleveland, OH"
+            else:
+                loc_addr += ", OH"
+        return {
+            "name": loc_name,
+            "address": loc_addr,
+        }
---
+++ tests/test_cuya_emergency_services_advisory.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import COMMITTEE, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.cuya_emergency_services_advisory import (
+    CuyaEmergencyServicesAdvisorySpider
+)
+
+test_response = file_response(
+    join(dirname(__file__), "files", "cuya_emergency_services_advisory.html"),
+    url="http://bc.cuyahogacounty.us/en-US/CC-EmergencySrvcsAdvsryBrd.aspx",
+)
+test_detail_response = file_response(
+    join(dirname(__file__), "files", "cuya_emergency_services_advisory_detail.html"),
+    url="http://bc.cuyahogacounty.us/en-US/091119-CCESAB-Comms-meeting.aspx",
+)
+spider = CuyaEmergencyServicesAdvisorySpider()
+
+freezer = freeze_time("2019-09-25")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+parsed_item = [item for item in spider._parse_detail(test_detail_response)][0]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 30
+
+
+def test_title():
+    assert parsed_item["title"] == "CCESAB Communications Committee"
+
+
+def test_description():
+    assert parsed_item["description"] == ""
+
+
+def test_start():
+    assert parsed_item["start"] == datetime(2019, 9, 11, 10, 15)
+
+
+def test_end():
+    assert parsed_item["end"] == datetime(2019, 9, 11, 11, 30)
+
+
+def test_time_notes():
+    assert parsed_item["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_item[
+        "id"] == "cuya_emergency_services_advisory/201909111015/x/ccesab_communications_committee"
+
+
+def test_status():
+    assert parsed_item["status"] == PASSED
+
+
+def test_location():
+    assert parsed_item["location"] == {
+        "name": "The Cassidy Theatre",
+        "address": "6200 Pearl Road Parma Heights, OH 44130"
+    }
+
+
+def test_source():
+    assert parsed_item["source"] == test_detail_response.url
+
+
+def test_links():
+    assert parsed_item["links"] == [{
+        "href": "http://bc.cuyahogacounty.us/ViewFile.aspx?file=7DSCAKoM0rqkeTzD%2f6%2f4cw%3d%3d",
+        "title": "Agenda"
+    }]
+
+
+def test_classification():
+    assert parsed_item["classification"] == COMMITTEE
+
+
+def test_all_day():
+    assert parsed_item["all_day"] is False
