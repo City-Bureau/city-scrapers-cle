596061013cfb607c4270192f70e6ece06e74fde3
pjsier <pjsier@gmail.com>
2019-09-25 15:28:05 -0500

    Add cuya_tax_incentive_review

---
+++ city_scrapers/spiders/cuya_tax_incentive_review.py
@@ -0,0 +1,92 @@
+import re
+from datetime import datetime
+
+from city_scrapers_core.constants import ADVISORY_COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class CuyaTaxIncentiveReviewSpider(CityScrapersSpider):
+    name = "cuya_tax_incentive_review"
+    agency = "Cuyahoga County Tax Incentive Review Council"
+    timezone = "America/Detroit"
+    allowed_domains = ["bc.cuyahogacounty.us"]
+    start_urls = ["http://bc.cuyahogacounty.us/en-US/Tax-Incentive-Review-Council.aspx"]
+    location = {
+        "name": "Cuyahoga County Headquarters",
+        "address": "2079 East 9th Street, Room 2-114, Cleveland, OH 44115",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for item in response.css("#contentColumn table")[-1:].css("tr")[1:]:
+            start = self._parse_start(item)
+            if not start:
+                continue
+            meeting = Meeting(
+                title=self._parse_title(item),
+                description="",
+                classification=ADVISORY_COMMITTEE,
+                start=self._parse_start(item),
+                end=None,
+                all_day=False,
+                time_notes="",
+                location=self._parse_location(item),
+                links=self._parse_links(item, response),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        return "Tax Incentive Review Council - " + " ".join(
+            item.css("td:nth-child(3) *::text").extract()
+        ).strip()
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        date_str = " ".join(item.css("td:first-child *::text").extract()).strip()
+        if not date_str:
+            return
+        time_el_str = " ".join(item.css("td:nth-child(2) *::text").extract()).strip()
+        time_str = "12:00am"
+        if time_el_str:
+            time_str = time_el_str
+        return datetime.strptime(date_str + time_str, "%m/%d/%y%I:%M%p")
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        loc_str = " ".join(item.css("td:nth-child(4) *::text").extract())
+        if "2079" in loc_str:
+            return self.location
+        split_loc = re.split(r", (?=\d{2})", loc_str, 1)
+        loc_name = ""
+        if len(split_loc) > 1:
+            loc_name, loc_addr = split_loc[0]
+        else:
+            loc_addr = split_loc[0]
+        if "Cleveland" not in loc_addr:
+            loc_addr += " Cleveland, OH"
+        return {
+            "name": loc_name,
+            "address": loc_addr,
+        }
+
+    def _parse_links(self, item, response):
+        """Parse or generate links."""
+        links = []
+        for link in item.css("a"):
+            links.append({
+                "title": " ".join(link.css("*::text").extract()).strip(),
+                "href": response.urljoin(link.attrib["href"]),
+            })
+        return links
---
+++ tests/test_cuya_tax_incentive_review.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import ADVISORY_COMMITTEE, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.cuya_tax_incentive_review import CuyaTaxIncentiveReviewSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "cuya_tax_incentive_review.html"),
+    url="http://bc.cuyahogacounty.us/en-US/Tax-Incentive-Review-Council.aspx",
+)
+spider = CuyaTaxIncentiveReviewSpider()
+
+freezer = freeze_time("2019-09-25")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 31
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Tax Incentive Review Council - Broadview Hts."
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 7, 16, 13, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0][
+        "id"
+    ] == "cuya_tax_incentive_review/201907161300/x/tax_incentive_review_council_broadview_hts_"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == test_response.url
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == ADVISORY_COMMITTEE
+
+
+def test_all_day():
+    assert parsed_items[0]["all_day"] is False
