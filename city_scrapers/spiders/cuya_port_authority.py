2042dec285f1dd4b23db229dad375daef1fb7881
pjsier <pjsier@gmail.com>
2019-10-14 08:59:29 -0500

    Add cuya_port_authority

---
+++ city_scrapers/spiders/cuya_port_authority.py
@@ -0,0 +1,112 @@
+import re
+from datetime import datetime
+from math import ceil
+
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from scrapy import Selector
+
+
+class CuyaPortAuthoritySpider(CityScrapersSpider):
+    name = "cuya_port_authority"
+    agency = "Cleveland-Cuyahoga County Port Authority"
+    timezone = "America/Detroit"
+    start_urls = ["http://www.portofcleveland.com/about-the-port/board-meeting-information/"]
+    location = {
+        "name": "Port of Cleveland Offices",
+        "address": "1100 W 9th St, Suite 100, Cleveland, OH 44113",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        self._validate_location(response)
+        self._validate_start_time(response)
+
+        # Go through 2 most recent years of meetings
+        for row in response.css("article table")[:2].css("tr"):
+            items = []
+            # If row is split into paragraphs, add as separate items
+            if len(row.css("td:first-child p")) > 0:
+                cell_list = []
+                half_split = ceil(len(row.css("p, td > a")) / 2)
+                for idx, el in enumerate(row.css("p, td > a")):
+                    if idx < half_split:
+                        el_str = el.extract()
+                        if isinstance(el_str, list):
+                            el_str = " ".join(el.extract())
+                        cell_list.append(el_str)
+                    else:
+                        cell_idx = idx - half_split
+                        el_str = el.extract()
+                        if isinstance(el_str, list):
+                            el_str = " ".join(el.extract())
+                        cell_list[cell_idx] += el_str
+                for cell in cell_list:
+                    print(cell)
+                    items.append(Selector(text=cell))
+            else:
+                items.append(row)
+
+            for item in items:
+                item_str = " ".join(item.css("*::text").extract())
+                start = self._parse_start(item_str)
+                if not start:
+                    continue
+
+                meeting = Meeting(
+                    title=self._parse_title(item_str),
+                    description="",
+                    classification=BOARD,
+                    start=start,
+                    end=None,
+                    all_day=False,
+                    time_notes="",
+                    location=self.location,
+                    links=self._parse_links(item, response),
+                    source=response.url,
+                )
+
+                meeting["status"] = self._get_status(meeting, text=item_str)
+                meeting["id"] = self._get_id(meeting)
+
+                yield meeting
+
+    def _parse_title(self, item_str):
+        """Parse or generate meeting title."""
+        if "Special" in item_str:
+            return "Board of Directors Special Meeting"
+        return "Board of Directors"
+
+    def _validate_start_time(self, response):
+        description = " ".join(response.css("article p *::text").extract())
+        if "8:30" not in description:
+            raise ValueError("Meeting start time has changed")
+
+    def _parse_start(self, item_str):
+        """Parse start datetime as a naive datetime object."""
+        date_match = re.search(r"[A-Z][a-z]{2,8} \d{1,2},? \d{4}", re.sub(r"\s+", " ", item_str))
+        if not date_match:
+            return
+        return datetime.strptime(date_match.group().replace(",", "") + " 8:30", "%B %d %Y %H:%M")
+
+    def _validate_location(self, response):
+        """Check if location has changed"""
+        description = " ".join(response.css("article p *::text").extract())
+        if "1100" not in description:
+            raise ValueError("Meeting location has changed")
+
+    def _parse_links(self, item, response):
+        """Parse or generate links."""
+        links = []
+        for link in item.css("a"):
+            links.append({
+                "title": " ".join(link.css("*::text").extract()).strip(),
+                "href": response.urljoin(link.attrib["href"]),
+            })
+        return links
---
+++ tests/test_cuya_port_authority.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import BOARD, CANCELLED, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.cuya_port_authority import CuyaPortAuthoritySpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "cuya_port_authority.html"),
+    url="http://www.portofcleveland.com/about-the-port/board-meeting-information/",
+)
+spider = CuyaPortAuthoritySpider()
+
+freezer = freeze_time("2019-10-14")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 23
+
+
+def test_title():
+    assert parsed_items[5]["title"] == "Board of Directors Special Meeting"
+
+
+def test_description():
+    assert parsed_items[5]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[5]["start"] == datetime(2019, 5, 22, 8, 30)
+
+
+def test_end():
+    assert parsed_items[5]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[5]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[5][
+        "id"] == "cuya_port_authority/201905220830/x/board_of_directors_special_meeting"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == CANCELLED
+    assert parsed_items[5]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[5]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[5]["source"] == test_response.url
+
+
+def test_links():
+    assert parsed_items[5]["links"] == [{
+        "href": "http://www.portofcleveland.com/media/1346/minutes-05222019.pdf",
+        "title": "Minutes"
+    }]
+
+
+def test_classification():
+    assert parsed_items[5]["classification"] == BOARD
+
+
+def test_all_day():
+    assert parsed_items[5]["all_day"] is False
