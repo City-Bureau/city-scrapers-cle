46cf904f87f7c78fe2733eafc4ac97a68ce47d02
pjsier <pjsier@gmail.com>
2020-06-12 17:59:38 +0000

    Handle virtual cuya_planning

--- city_scrapers/spiders/cuya_planning.py
+++ city_scrapers/spiders/cuya_planning.py
@@ -66,7 +66,7 @@ class CuyaPlanningSpider(CityScrapersSpider):
 
     def _validate_location(self, response):
         desc_str = " ".join(response.css(".entry-content .large-12 *::text").extract())
-        if "4-407" not in desc_str:
+        if "4-407" not in desc_str and "virtually" not in desc_str:
             raise ValueError("Meeting location has changed")
 
     def _parse_links(self, response):
