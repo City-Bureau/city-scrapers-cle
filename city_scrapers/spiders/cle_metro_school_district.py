2aeb6167a41f70a7d12030d757b304f7b687b794
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-11-25 01:55:48 -0500

    Make compatible with new site format

--- city_scrapers/spiders/cle_metro_school_district.py
+++ city_scrapers/spiders/cle_metro_school_district.py
@@ -1,7 +1,7 @@
-import re
 from datetime import datetime
-
-from city_scrapers_core.constants import BOARD, FORUM
+import json
+from requests import Session
+from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 
@@ -10,90 +10,79 @@ class CleMetroSchoolDistrictSpider(CityScrapersSpider):
     name = "cle_metro_school_district"
     agency = "Cleveland Metropolitan School District"
     timezone = "America/Detroit"
-    start_urls = ["https://www.boarddocs.com/oh/cmsd/board.nsf/XML-ActiveMeetings"]
+    start_urls = ["https://go.boarddocs.com/oh/cmsd/Board.nsf/Public"]
 
     def parse(self, response):
-        """
-        `parse` should always `yield` Meeting items.
+        session = Session()
+        json_response = session.post(
+            url='https://go.boarddocs.com/oh/cmsd/Board.nsf/BD-GetMeetingsList?open&0.01411260163957615',
+            data={'current_committee_id': 'A9HCRJ3251CA'}
+        )
+        meetings = json.loads(json_response.text)
 
-        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
-        needs.
         """
-        for item in response.xpath("//meeting"):
-            agenda_url = item.xpath("./link/text()").extract_first()
-            links = []
-            if agenda_url:
-                links = [{"title": "Agenda", "href": agenda_url}]
+        Sample JSON item
+
+        {'current': '',
+        'name': 'Cleveland Municipal School District Work Session Meeting - 6:30 '
+                'P.M.',
+        'numberdate': '20090210',
+        'preliveoak': '1',
+        'unid': '49915970A892CAF2852576F5001D114D',
+        'unique': '842LYP1D114D'}
+        """
+
+        for m in meetings:
             meeting = Meeting(
-                title=self._parse_title(item),
+                title=self._parse_title(m),
                 description="",
-                classification=self._parse_classification(item),
-                start=self._parse_start(item),
+                classification=self._parse_classification(m),
+                start=self._parse_start(m),
                 end=None,
                 all_day=False,
                 time_notes="",
-                location=self._parse_location(item),
-                links=links,
-                source=agenda_url or response.url,
+                location="",
+                links=self._parse_links(m),
+                source=self._parse_source(response),
             )
-
             meeting["status"] = self._get_status(meeting)
             meeting["id"] = self._get_id(meeting)
 
             yield meeting
 
-    def _parse_title(self, item):
+    def _parse_title(self, m):
         """Parse or generate meeting title."""
-        title_str = (
-            item.xpath("./name/text()")
-            .extract_first()
-            .replace("Cleveland Municipal School District ", "")
-        )
-        return "-".join(title_str.split("-")[:-1]).strip()
-
-    def _parse_classification(self, item):
-        """Parse or generate classification from allowed options."""
-        title_str = item.xpath("./name/text()").extract_first()
-        if "Community" in title_str:
-            return FORUM
-        return BOARD
-
-    def _parse_start(self, item):
-        """Parse start datetime as a naive datetime object."""
-        title_str = item.xpath("./name/text()").extract_first()
-        time_str = "12:00 AM"
-        time_match = re.search(r"\d{1,2}:\d{1,2} *[APM\.]{2,4}", title_str)
-        if time_match:
-            time_str = time_match.group().replace(".", "")
-        date_str = item.xpath("./start/date/text()").extract_first()
-        return datetime.strptime(" ".join([date_str, time_str]), "%Y-%m-%d %I:%M %p")
-
-    def _parse_location(self, item):
-        """Parse or generate location."""
-        loc_item = (
-            item.xpath("./category[@order='1']/agendaitems/item/name/text()")
-            .extract_first()
-            .strip()
-        )
-        loc_str = re.sub(r"^\d{1,2}\.\d{1,2} ?", "", loc_item)
-        loc_parts = re.split(r", ?(?=\d{2})", loc_str, 1)
-        if len(loc_parts) == 2:
-            return {
-                "address": loc_parts[1],
-                "name": loc_parts[0],
-            }
-        if "Board of Education Administration" in loc_parts[0]:
-            return {
-                "address": "1111 Superior Ave E, Cleveland, OH 44114",
-                "name": loc_parts[0],
-            }
-        if re.search(r"^\d{2,4}", loc_parts[0]):
-            return {
-                "address": loc_parts[0],
-                "name": "",
-            }
+        title = m.get("name")
+        if title:
+            title = title.rsplit(" -", 1)[0]
+            return title
         else:
-            return {
-                "address": "",
-                "name": loc_parts[0],
-            }
+            return ""
+
+    def _parse_start(self, m):
+        """Parse start datetime as a naive datetime object."""
+        date_str = m.get("numberdate")
+        time_str = m.get("name")
+        if time_str:
+            time_str = time_str.rsplit("- ", 1)[1]
+            time_str = time_str.replace(".", "")
+
+        date_obj = datetime.strptime(f'{date_str} {time_str}', "%Y%m%d %I:%M %p")
+        return date_obj
+
+    def _parse_classification(self, m):
+        """Parse or generate classification from allowed options."""
+        return NOT_CLASSIFIED
+
+    def _parse_links(self, m):
+        """Parse or generate links."""
+        meeting_id = m.get("unique")
+        if meeting_id:
+            url = "https://go.boarddocs.com/oh/cmsd/Board.nsf/goto?open&id=" + meeting_id
+            return [{"href": url, "title": f"{self._parse_title(m)}"}]
+        else:
+            return []
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
