3839e39863246e441608e5f862284a2f02e2f3b3
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-01-12 06:13:21 -0600

    Replace scrapy-sentry with city-scraper-sentry

--- city_scrapers/settings/prod.py
+++ city_scrapers/settings/prod.py
@@ -18,7 +18,7 @@ SENTRY_DSN = os.getenv("SENTRY_DSN")
 
 EXTENSIONS = {
     "city_scrapers_core.extensions.AzureBlobStatusExtension": 100,
-    "scrapy_sentry.extensions.Errors": 10,
+    "city_scrapers_sentry.extensions.Errors": 10,
     "scrapy.extensions.closespider.CloseSpider": None,
 }
 
