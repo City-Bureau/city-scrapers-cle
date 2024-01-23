bb1c33f95beb8f55324b4e8055113cd606fd7641
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-01-23 06:40:50 -0600

    Rework spider with new mixin

--- city_scrapers/spiders/cuya_archives_advisory.py
+++ city_scrapers/spiders/cuya_archives_advisory.py
@@ -1,21 +1,13 @@
 from city_scrapers_core.constants import ADVISORY_COMMITTEE
 from city_scrapers_core.spiders import CityScrapersSpider
 
-from city_scrapers.mixins import CuyaCountyMixin
+from city_scrapers.mixins import CuyaCountyMixin2
 
 
-class CuyaArchivesAdvisorySpider(CuyaCountyMixin, CityScrapersSpider):
+class CuyaArchivesAdvisorySpider(CuyaCountyMixin2, CityScrapersSpider):
     name = "cuya_archives_advisory"
     agency = "Cuyahoga County Archives Advisory Commission"
-    start_urls = ["http://bc.cuyahogacounty.us/en-US/Archives-Advisory-Commission.aspx"]
+    start_urls = [
+        "https://cuyahogacounty.gov/boards-and-commissions/board-details/external/archives-advisory-commission"
+    ]  # noqa
     classification = ADVISORY_COMMITTEE
-    location = {
-        "name": "Cuyahoga County Archives Building, 3rd Floor",
-        "address": "3951 Perkins Ave Cleveland, OH 44114",
-    }
-
-    def _parse_location(self, response):
-        loc_str = super()._parse_location(response)
-        if "Perkins" in loc_str:
-            return self.location
-        return {"name": "", "address": loc_str}
