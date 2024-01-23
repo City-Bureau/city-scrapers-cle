b4ad8e24e19ebcb4d12ee135f864bc6fbd33bbdb
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-01-23 06:05:56 -0600

    Update tests for rebuilt spider

--- tests/test_cuya_children_family_advisory.py
+++ tests/test_cuya_children_family_advisory.py
@@ -12,15 +12,15 @@ from city_scrapers.spiders.cuya_children_family_advisory import (
 
 test_response = file_response(
     join(dirname(__file__), "files", "cuya_children_family_advisory.html"),
-    url="http://bc.cuyahogacounty.us/en-US/Children-Family-Services-Advisory-Board.aspx",  # noqa
+    url="https://cuyahogacounty.gov/boards-and-commissions/board-details/external/children-and-family-services-planning-committee",  # noqa
 )
 test_detail_response = file_response(
     join(dirname(__file__), "files", "cuya_children_family_advisory_detail.html"),
-    url="http://bc.cuyahogacounty.us/en-US/040319-DCFS-Advisory-Board.aspx",
+    url="https://cuyahogacounty.gov/boards-and-commissions/bc-event-detail//2023/06/07/boards-and-commissions/060723dcfsadvisoryboard_a01bf9a3-a56e-4d64-93af-58a7c46fd1d6",  # noqa
 )
 spider = CuyaChildrenFamilyAdvisorySpider()
 
-freezer = freeze_time("2019-09-16")
+freezer = freeze_time("2024-01-20")
 freezer.start()
 
 parsed_items = [item for item in spider.parse(test_response)]
@@ -30,23 +30,26 @@ freezer.stop()
 
 
 def test_count():
-    assert len(parsed_items) == 3
+    assert len(parsed_items) == 6
 
 
 def test_title():
-    assert parsed_item["title"] == "DCFS Advisory Board"
+    assert parsed_item["title"] == "06/07/23 DCFS Advisory Board"
 
 
 def test_description():
-    assert parsed_item["description"] == ""
+    assert (
+        parsed_item["description"]
+        == "Join the Teams meeting . Meeting ID: 227 002 759 696 Passcode: xw4Pza"
+    )
 
 
 def test_start():
-    assert parsed_item["start"] == datetime(2019, 4, 3, 16, 0)
+    assert parsed_item["start"] == datetime(2023, 6, 7, 18, 0)
 
 
 def test_end():
-    assert parsed_item["end"] == datetime(2019, 4, 3, 18, 0)
+    assert parsed_item["end"] is None
 
 
 def test_time_notes():
@@ -56,7 +59,7 @@ def test_time_notes():
 def test_id():
     assert (
         parsed_item["id"]
-        == "cuya_children_family_advisory/201904031600/x/dcfs_advisory_board"
+        == "cuya_children_family_advisory/202306071800/x/06_07_23_dcfs_advisory_board"
     )
 
 
@@ -67,23 +70,27 @@ def test_status():
 def test_location():
     assert parsed_item["location"] == {
         "name": "",
-        "address": "3955 Euclid Avenue, Room #348E Cleveland, OH",
+        "address": "MS Teams Meeting",
     }
 
 
 def test_source():
     assert (
         parsed_item["source"]
-        == "http://bc.cuyahogacounty.us/en-US/040319-DCFS-Advisory-Board.aspx"
+        == "https://cuyahogacounty.gov/boards-and-commissions/bc-event-detail//2023/06/07/boards-and-commissions/060723dcfsadvisoryboard_a01bf9a3-a56e-4d64-93af-58a7c46fd1d6"  # noqa
     )
 
 
 def test_links():
     assert parsed_item["links"] == [
         {
-            "href": "http://bc.cuyahogacounty.us/ViewFile.aspx?file=YICdz%2bxQNqQfQk8n9mX1vA%3d%3d",  # noqa
+            "href": "https://cuyahogacms.blob.core.windows.net/home/docs/default-source/boards-and-commissions/external/cfs/2023/060723-cfsagenda.pdf?sfvrsn=f168e538_1",  # noqa
+            "title": "Agenda",
+        },
+        {
+            "href": "https://cuyahogacms.blob.core.windows.net/home/docs/default-source/boards-and-commissions/external/cfs/2023/060723-cfsminutes.pdf?sfvrsn=560614ef_1",  # noqa
             "title": "Minutes",
-        }
+        },
     ]
 
 
