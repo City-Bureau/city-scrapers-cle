ef69059a623fcb47eb79a0b3ca3db79a007ee79f
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-01-23 19:31:16 -0600

    Fix cuya_regional_data_sharing spider

--- city_scrapers/spiders/cuya_regional_data_sharing.py
+++ city_scrapers/spiders/cuya_regional_data_sharing.py
@@ -1,27 +1,13 @@
-import re
-
 from city_scrapers_core.constants import BOARD
 from city_scrapers_core.spiders import CityScrapersSpider
 
-from city_scrapers.mixins import CuyaCountyMixin
+from city_scrapers.mixins import CuyaCountyMixin2
 
 
-class CuyaRegionalDataSharingSpider(CuyaCountyMixin, CityScrapersSpider):
+class CuyaRegionalDataSharingSpider(CuyaCountyMixin2, CityScrapersSpider):
     name = "cuya_regional_data_sharing"
     agency = "Cuyahoga County Regional Enterprise Data Sharing System"
-    start_urls = ["http://bc.cuyahogacounty.us/en-US/CRIS.aspx"]
+    start_urls = [
+        "https://cuyahogacounty.gov/boards-and-commissions/board-details/other/regional-data-enterprise-sharing-system"  # noqa
+    ]
     classification = BOARD
-    location = {
-        "name": "County Headquarters, 5th Floor Rm. 5-006",
-        "address": "2079 East 9th St Cleveland, OH 44115",
-    }
-
-    def _parse_title(self, response):
-        return "Governing Board"
-
-    def _parse_location(self, response):
-        loc_str = super()._parse_location(response)
-        if "5-006" in loc_str:
-            return self.location
-        split_loc = re.split(r" (?=2079)", loc_str)
-        return {"name": "", "address": " ".join(split_loc[1:])}
--- tests/test_cuya_regional_data_sharing.py
+++ tests/test_cuya_regional_data_sharing.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest  # noqa
-from city_scrapers_core.constants import BOARD, PASSED
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -12,15 +11,15 @@ from city_scrapers.spiders.cuya_regional_data_sharing import (
 
 test_response = file_response(
     join(dirname(__file__), "files", "cuya_regional_data_sharing.html"),
-    url="http://bc.cuyahogacounty.us/en-US/CRIS.aspx",
+    url="https://cuyahogacounty.gov/boards-and-commissions/board-details/other/regional-data-enterprise-sharing-system",  # noqa
 )
 test_detail_response = file_response(
     join(dirname(__file__), "files", "cuya_regional_data_sharing_detail.html"),
-    url="http://bc.cuyahogacounty.us/en-US/040519-REDSS-meeting.aspx",
+    url="https://cuyahogacounty.gov/boards-and-commissions/bc-event-detail//2023/10/19/boards-and-commissions/redss-board-meeting---10-19-23",  # noqa
 )
 spider = CuyaRegionalDataSharingSpider()
 
-freezer = freeze_time("2019-10-03")
+freezer = freeze_time("2024-1-16")
 freezer.start()
 
 parsed_items = [item for item in spider.parse(test_response)]
@@ -30,11 +29,11 @@ freezer.stop()
 
 
 def test_count():
-    assert len(parsed_items) == 3
+    assert len(parsed_items) == 4
 
 
 def test_title():
-    assert parsed_item["title"] == "Governing Board"
+    assert parsed_item["title"] == "REDSS Board Meeting - 10/19/23"
 
 
 def test_description():
@@ -42,11 +41,11 @@ def test_description():
 
 
 def test_start():
-    assert parsed_item["start"] == datetime(2019, 4, 5, 14, 0)
+    assert parsed_item["start"] == datetime(2023, 10, 19, 13, 0)
 
 
 def test_end():
-    assert parsed_item["end"] == datetime(2019, 4, 5, 15, 0)
+    assert parsed_item["end"] == datetime(2023, 10, 19, 14, 0)
 
 
 def test_time_notes():
@@ -55,37 +54,40 @@ def test_time_notes():
 
 def test_id():
     assert (
-        parsed_item["id"] == "cuya_regional_data_sharing/201904051400/x/governing_board"
+        parsed_item["id"]
+        == "cuya_regional_data_sharing/202310191300/x/redss_board_meeting_10_19_23"  # noqa
     )
 
 
 def test_status():
-    assert parsed_item["status"] == PASSED
+    assert parsed_item["status"] == "passed"
 
 
 def test_location():
-    assert parsed_item["location"] == spider.location
+    assert parsed_item["location"] == {
+        "name": "",
+        "address": "2079 East 9th St. Cleveland, OH 44115 - Room 4-407",
+    }
 
 
 def test_source():
-    assert parsed_item["source"] == test_detail_response.url
+    assert (
+        parsed_item["source"]
+        == "https://cuyahogacounty.gov/boards-and-commissions/bc-event-detail//2023/10/19/boards-and-commissions/redss-board-meeting---10-19-23"  # noqa
+    )
 
 
 def test_links():
     assert parsed_item["links"] == [
         {
-            "href": "http://bc.cuyahogacounty.us/ViewFile.aspx?file=yC5L9gSioQrD%2bnXtN%2bErfg%3d%3d",  # noqa
+            "href": "https://cuyahogacms.blob.core.windows.net/home/docs/default-source/agendas/101923-redssmeetingmin.pdf?sfvrsn=5aa73ec9_1",  # noqa
             "title": "Minutes",
-        },
-        {
-            "href": "http://bc.cuyahogacounty.us/ViewFile.aspx?file=yC5L9gSioQr6HqvCS4du4g%3d%3d",  # noqa
-            "title": "Agenda",
-        },
+        }
     ]
 
 
 def test_classification():
-    assert parsed_item["classification"] == BOARD
+    assert parsed_item["classification"] == "Board"
 
 
 def test_all_day():
